e4a9f6f1a571918f73e11dd93123b66226857010
Abdullah Zahra <abdullahmzahra@gmail.com>
2023-04-01 20:22:03 +0200

    MDay 72

--- routes/demo.js
+++ routes/demo.js
@@ -10,7 +10,18 @@ router.get("/", function (req, res) {
 });
 
 router.get("/signup", function (req, res) {
-  res.render("signup");
+  let sessionInputData = req.session.inputData;
+
+  if (!sessionInputData) {
+    sessionInputData = {
+      hassError: false,
+      email: "",
+      confirmEmail: "",
+      password: "",
+    };
+  }
+
+  res.render("signup", { inputData: sessionInputData });
 });
 
 router.get("/login", function (req, res) {
@@ -31,8 +42,18 @@ router.post("/signup", async function (req, res) {
     enteredEmail !== enteredConfirmEmail ||
     !enteredEmail.includes("@")
   ) {
-    console.log("Incorrect data");
-    return res.redirect("/signup");
+    req.session.inputData = {
+      hasError: true,
+      message: "Invalid input - please check your data.",
+      email: enteredEmail,
+      confirmEmail: enteredConfirmEmail,
+      password: enteredPassword,
+    };
+
+    res.session.save(function () {
+      res.redirect("/signup");
+    });
+    return;
   }
 
   const existingUser = await db
